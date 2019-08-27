d294f9a0d5fa2da61794e43bb360f49acaf1c019
Juan Gallardo <00091813@uca.edu.sv>
2019-08-27 07:43:25 -0600

    Cuarta actividad

---
+++ cuartaActividad.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+typedef struct pais
+{
+	string nombre;
+	string capital;
+	int habitantes;
+	float mediaEdad;	
+} pais;
+
+int main(void)
+{
+	pais Continente[5][5];
+	return 0;
+}
